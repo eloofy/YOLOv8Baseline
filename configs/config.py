55d6eb38931d8cb8ddade573ec9e0ed946466e02
anatoly <t.vlasov23@mail.ru>
2023-10-23 10:08:48 +0300

    Update config.

---
+++ configs/config.py
@@ -0,0 +1,20 @@
+from pathlib import Path
+
+home_path = Path(__file__).parent.parent
+seed = 12345
+
+train_parameters = {
+    'model_path': '../yolov8n-seg.pt',
+    'data_config': 'data.yaml',
+    'img_size': 512,
+    'epochs': 1,
+    'patience': 100,
+    'batch_size': 8,
+    'device': 0,
+    'workers': 12,
+    'optimizer': 'AdamW',
+    'learning_rate': 0.001,
+    'model_name': 'yolov8n_custom',
+    'cos_lr': True,
+    'show_labels': False
+}
