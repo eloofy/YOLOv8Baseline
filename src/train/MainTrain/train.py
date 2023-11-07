7b00c2e6a2ecd4d8c43d08decb43dbb1c6f2fd15
eloofy <t.vlasov23@mail.ru>
2023-11-07 09:31:38 +0000

    Update inference and train.

--- src/train/MainTrain/train.py
+++ src/train/MainTrain/train.py
@@ -4,27 +4,30 @@ from ultralytics import YOLO
 import mlflow
 from ultralytics import settings
 from src.train.MLflowTracking.cust_mlflow import MLflowTracking
-
 from configs.config import home_path
-settings.update({"mlflow": False})
+
+
+MLFLOW_TRACKING_URI = "http://mlflow:5000"
+EXPERIMENT_NAME = "YOLOv8BaseLineTrain"
+MODEL_CONFIG_FILE = "configs/model_cfg.yaml"
+RESULTS_DIR = os.path.join(home_path, "src/train/MainTrain/results/")
+
+settings.update({"mlflow": False, "runs_dir": RESULTS_DIR})
 
 
 class YOLOTrainer:
     def __init__(self, tracking_uri: str, experiment_name: str, cfg_model: str):
         """
         Initialize the YOLOTrainer.
+
         Args:
             tracking_uri: The MLflow tracking URI.
             experiment_name: Name of the MLflow experiment.
         """
-        self.model = YOLO(
-            self._load_model_config(cfg_model)["training_params"]["model"]
-        )
+        self.cfg_file = self._load_model_config(cfg_model)
+        self.model = YOLO(self.cfg_file["training_params"]["model"])
         # self.model.callbacks = {...} Add custom func
         self.mlflow_tracking = MLflowTracking(tracking_uri, experiment_name)
-
-    # @staticmethod
-    # def _load_dataset_description(count_datasets):
 
     @staticmethod
     def _load_model_config(model_cfg_file: str):
@@ -49,33 +52,25 @@ class YOLOTrainer:
         """
         with mlflow.start_run(
             run_name="YOLOv8_ver",
-            description=self.mlflow_tracking.load_description(
+            description=self.mlflow_tracking.load_dataset_description(
                 file_path=os.path.join(home_path, "src/train/MainTrain/data.yaml")
-            )
+            ),
         ):
             self.model.train(**model_config["training_params"])
             self.mlflow_tracking.set_all_params(self.model, model_config)
 
-
-    def run_training(self, model_cfg_file: str):
+    def run_training(self):
         """
         Run the YOLO model training process.
 
-        Args:
-            model_cfg_file: Path to the YOLO model configuration file.
         """
-        model_config = self._load_model_config(model_cfg_file)
-        self._train_yolo_model(model_config)
+        self._train_yolo_model(self.cfg_file)
 
 
 def main():
-    model_cfg_path = "configs/model_cfg.yaml"
+    trainer = YOLOTrainer(MLFLOW_TRACKING_URI, EXPERIMENT_NAME, MODEL_CONFIG_FILE)
 
-    trainer = YOLOTrainer("http://neuron:5000",
-                          "YOLOv8BaseLineTrain",
-                          model_cfg_path)
-
-    trainer.run_training(model_cfg_path)
+    trainer.run_training()
 
 
 if __name__ == "__main__":
